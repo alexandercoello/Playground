e66ea21311caf604270591b5a40b9821bbb5e313
Alexander Coello <alexandercoello1@gmail.com>
2026-08-16 07:58:55 -0400

    Added Todo in Interactable Class

--- Assets/Scripts/Objects/Base/Interactable.cs
+++ Assets/Scripts/Objects/Base/Interactable.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 
 namespace Scripts.Objects.Base
 {
+
+    /// <summary>
+    /// Base Class Object that a player is able to Intertact with using their Interact button.
+    /// </summary>
+    /// TODO: Refactor out the Dynamic SceneObjectController to simplify this class
     public abstract class Interactable : MonoBehaviour //DynamicSceneObjectController
     {
         public List<GameObject> LinkedGameObjects;
