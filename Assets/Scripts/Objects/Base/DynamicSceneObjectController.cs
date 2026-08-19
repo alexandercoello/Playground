35501662ace057e37656cffb5f445850f0942156
Alexander Coello <alexandercoello1@gmail.com>
2026-08-19 09:00:19 -0400

    Add in sounds for button and lever

--- Assets/Scripts/Objects/Base/DynamicSceneObjectController.cs
+++ Assets/Scripts/Objects/Base/DynamicSceneObjectController.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Scripts.Objects.Base
 {
@@ -17,12 +20,40 @@ namespace Scripts.Objects.Base
             set
             {
                 isActive = value;
-
                 OnIsActiveChanged();
+
+                PlaySound();
             }
         }
 
+        public List<AudioClip> ActivateAudioClips;
+        public List<AudioClip> DeactivateAudioClips;
+        AudioSource audioSource;        
+
         public abstract void OnIsActiveChanged();
 
+        void PlaySound()
+        {
+            if(audioSource.IsUnityNull())
+            {
+                audioSource = GetComponent<AudioSource>();               
+            }
+
+            if(isActive && !ActivateAudioClips.IsUnityNull() && ActivateAudioClips.Count != 0)
+            {
+                //Play Activate Sound                
+                audioSource.PlayOneShot(PickRandomAudioClip(ActivateAudioClips));
+            }
+            else if(!isActive && !DeactivateAudioClips.IsUnityNull() && DeactivateAudioClips.Count != 0)
+            {
+                //Play DeactivateSound
+                audioSource.PlayOneShot(PickRandomAudioClip(DeactivateAudioClips));
+            }
+        }
+
+        AudioClip PickRandomAudioClip(List<AudioClip> clips)
+        {
+            return clips[Random.Range(0, clips.Count)];
+        }
     }
 }
